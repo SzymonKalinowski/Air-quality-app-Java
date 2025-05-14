2ed155a56e79b6363e87af4ab815a17fb6becc23
SzymonKalinowski <146426686+SzymonKalinowski@users.noreply.github.com>
2025-05-14 14:02:58 +0200

    przygotowanie wszystkiego

---
+++ Air-quality-app/src/main/java/com/example/airqualityapp/AirQualityAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.airqualityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirQualityAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirQualityAppApplication.class, args);
+    }
+
+}
---
+++ Air-quality-app/src/test/java/com/example/airqualityapp/AirQualityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airqualityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirQualityAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
