551b6de3fade47647c1238250309e059391e6a6a
Szymon <u122_szykal_waw@technischools.com>
2025-05-22 23:56:57 +0200

    zrobienie laczenia z backendem i dane pogodowe

--- Air-quality-app/src/main/java/com/example/airqualityapp/ClientAPI/clientAPI.java
+++ Air-quality-app/src/main/java/com/example/airqualityapp/ClientAPI/clientAPI.java
@@ -1,19 +1,42 @@
 package com.example.airqualityapp.ClientAPI;
 
 import java.io.IOException;
+import java.lang.classfile.components.ClassPrinter;
 import java.net.http.*;
 import java.net.URI;
-import org.json.*;
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 
 public class clientAPI {
     private static String latitude = "52.52";
     private static String longitude = "13.41";
 
     private static String name = "localhost";
-    private static String port = "8080";
+    private static Integer port = 8080;
 
 
-    public static JSONObject fetchAirQuality(String latitude, String longitude) throws IOException, InterruptedException {
+    private static final Gson gson = new Gson();
+
+    public static JsonObject fetchWeather() throws IOException, InterruptedException {
+        String url = "https://api.open-meteo.com/v1/forecast?latitude=52.2297&longitude=21.0122&current_weather=true&hourly=pressure_msl,relative_humidity_2m";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() != 200) {
+            throw new IOException("Api response: " + response.statusCode());
+        }
+
+        return JsonParser.parseString(response.body()).getAsJsonObject();
+    }
+
+    public static JsonObject fetchAirQuality(String latitude, String longitude) throws IOException, InterruptedException {
         String url = String.format(
                 "https://air-quality-api.open-meteo.com/v1/air-quality?latitude=%s&longitude=%s&hourly=pm10,pm2_5,carbon_monoxide,nitrogen_dioxide,sulphur_dioxide,ozone&timezone=Europe%%2FWarsaw",
                 latitude, longitude
@@ -28,9 +51,43 @@ public class clientAPI {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         if (response.statusCode() != 200) {
-            throw new IOException("API status " + response.statusCode());
+            throw new IOException("Api response: " + response.statusCode());
         }
 
-        return new JSONObject(response.body());
+        return JsonParser.parseString(response.body()).getAsJsonObject();
+    }
+
+    public static JsonObject sendToBackend(JsonObject reading, String backendName, Integer backendPort) throws IOException, InterruptedException {
+        String url = String.format("http://%s:%d/readings", backendName, backendPort);
+        String json = gson.toJson(reading);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url)).build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        JsonObject result = new JsonObject();
+        if (response.statusCode() != 200) {
+            throw new IOException("Backend response: " + response.statusCode());
+        }
+        result.add("body", JsonParser.parseString(response.body()));
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        try{
+            JsonObject weather = fetchWeather();
+            JsonObject data = fetchAirQuality(latitude,longitude);
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
