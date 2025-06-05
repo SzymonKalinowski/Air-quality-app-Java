b2882a1607a866a322a66b72f128c94b6c8be8dd
SzymonKalinowski <146426686+SzymonKalinowski@users.noreply.github.com>
2025-06-05 15:47:15 +0200

    userservice rozwinięcie

--- Air-quality-app/src/main/java/com/example/airqualityapp/Service/readingService.java
+++ Air-quality-app/src/main/java/com/example/airqualityapp/Service/readingService.java
@@ -2,7 +2,10 @@ package com.example.airqualityapp.Service;
 
 
 import com.example.airqualityapp.Model.Reading;
+import com.example.airqualityapp.Model.ReadingClosestTimestamp;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class readingService {
@@ -12,8 +15,29 @@ public class readingService {
         this.reading = reading;
     }
 
-    public Reading getClosestData(String date){
-        return reading;
+    public ReadingClosestTimestamp getClosestToTimestamp() {
+        String timestamp = reading.getTimestamp();
+        List<String> time = reading.getWeather().getHourly().getTime();
+        List<Double> pressure_msl = reading.getWeather().getHourly().getPressure_msl();
+        List<Integer> relative_humidity_2m = reading.getWeather().getHourly().getRelative_humidity_2m();
+
+        Integer index = -1;
+        if (time.contains(timestamp)) {
+            index = time.indexOf(timestamp);
+        }
+
+        String foundTime = time.get(index);
+        Double correspondingPressure = pressure_msl.get(index);
+        Integer correspondingHumidity = relative_humidity_2m.get(index);
+
+        ReadingClosestTimestamp.Hourly HourlyData = new ReadingClosestTimestamp.Hourly();
+        HourlyData.setTime(foundTime);
+        HourlyData.setPressure_msl(correspondingPressure);
+        HourlyData.setRelative_humidity_2m(correspondingHumidity);
+
+
+
+
     }
 
 
